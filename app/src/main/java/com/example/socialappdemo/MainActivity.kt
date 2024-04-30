e7657f15c4acb2cd66a635ff4d178b021b862882
Tajamul Arafath <vptajju@gmail.com>
2024-04-30 15:13:11 +0530

    Added Feature Bottom Navigation Bar

--- app/src/main/java/com/example/socialappdemo/MainActivity.kt
+++ app/src/main/java/com/example/socialappdemo/MainActivity.kt
@@ -2,10 +2,15 @@ package com.example.socialappdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import com.example.socialappdemo.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     private lateinit var binding : ActivityMainBinding
+    private lateinit var bottomNav: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
@@ -17,6 +22,42 @@ class MainActivity : AppCompatActivity() {
         binding.vfollowing.tvPostscount.setText("100")
         binding.vfollowing.posttext.setText("following")
 
+        val homeFragment = home_fragment()
+        val SearchFragment = SearchFragment()
+        val ProfileFragment = ProfileFragment()
 
+        loadFragment(homeFragment)
+        bottomNav = binding.bottomNav
+        bottomNav.setOnItemSelectedListener {
+            when(it.itemId){
+                R.id.home_button -> {
+                    loadFragment(homeFragment)
+                    true
+                }
+                R.id.search_button ->{
+                    loadFragment(SearchFragment)
+                    true
+                }
+                R.id.proflie_button ->{
+                    loadFragment(ProfileFragment)
+                    true
+                }
+
+                else -> {
+                    loadFragment(homeFragment)
+                    true
+                }
+            }
+        }
+
+
+
+
+    }
+
+    private fun loadFragment(fragment: Fragment) {
+        val transaction = supportFragmentManager.beginTransaction()
+        transaction.replace(R.id.bottonnavcontainer, fragment)
+        transaction.commit()
     }
 }
